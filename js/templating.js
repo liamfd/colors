4ed5d635928e22e54fd29d9dd10aa92155c2e00d
liamfd <liamfd@gmail.com>
2014-05-07 17:07:39 -0400

    underscore templating

--- js/templating.js
+++ js/templating.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
-	var source   = $("#my-template").html();
-	var template = Handlebars.compile(source);
+
+    _.templateSettings.variable = "rc";
+
+    // Grab the HTML out of our template tag and pre-compile it.
+    var template = _.template(
+        $( "script#my-template" ).html()
+    );
+
 	var data = { colors: [
 		{name: "red"},
 		{name: "blue"},
 		{name: "yellow"}
 	]};
-	$("#content").html(template(data));
+
+	$("#primary-colors").html(
+		template(data)
+	);
+
+/*	var source   = $("#my-template").html();
+	var template = Handlebars.compile(source);
+	console.log(source);
+	var data = { colors: [
+		{name: "red"},
+		{name: "blue"},
+		{name: "yellow"}
+	]};
+	$("#primary-color-list").html(template(data));
+*/
 });
