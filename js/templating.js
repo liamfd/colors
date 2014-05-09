bcf64033a48df235950599b654510bd4870d2437
liamfd <liamfd@gmail.com>
2014-05-08 22:15:40 -0400

    got the size change working automatically

--- js/templating.js
+++ js/templating.js
@@ -1,8 +1,8 @@
 var primary_color_data = { colors: [
-	{name: "red", val:"#990000"},
-	{name: "blue", val:"#000099"},
-	{name: "green", val:"#009900"},
-	{name: "yellow", val:"#999900"}
+	{name: "red", val:"#d41a33"},
+	{name: "blue", val:"#3854ad"},
+	{name: "green", val:"#37b33f"},
+	{name: "yellow", val:"#f5ee0a"}
 ]};
 
 var secondary_color_data = { colors: [
@@ -18,6 +18,8 @@ var secondary_color;
 
 $(document).ready(function() {
     _.templateSettings.variable = "rc";
+    var primary_color_group = $("#primary-colors");
+    var secondary_color_group = $("#secondary-colors");
 
     // Grab the HTML out of our template tag and pre-compile it.
     var template = _.template(
@@ -28,26 +30,41 @@ $(document).ready(function() {
     primary_color = primary_color_data.colors[0].val;
     secondary_color = secondary_color_data.colors[0].val;
 	setOppositeColor(primary_color, template);
-    setCenterCircle();
+    setCenterCircle(primary_color);
 
     //click template and set click listener for primary
-	$("#primary-colors").html(
+	primary_color_group.html(
 		template(primary_color_data)
 	).on("click",".color-circle", function(e){
 		var el = e.target;
 		primary_color = el.dataset.color;
+
+		//set visual styles of chosen color circles
+		primary_color_group.find(".color-circle").removeClass("large-color-circle");
+		$(el).addClass("large-color-circle");
+
+		//save the color, set the center circle color
 		setOppositeColor(primary_color, template);
-		setCenterCircle();
+		setCenterCircle(primary_color);
 	});
 
 	//template and set click listener for secondary
-	$("#secondary-colors").html(
+	secondary_color_group.html(
 		template(secondary_color_data)
 	).on("click",".color-circle", function(e){
 		var el = e.target;
 		secondary_color = el.dataset.color;
+
+		//set visual styles of chosen color circles
+		secondary_color_group.find(".color-circle").removeClass("large-color-circle");
+		$(el).addClass("large-color-circle");
 	});
 
+	//set default color options visually for the first elements of each list
+	primary_color_group.find(".color-circle").first().addClass("large-color-circle");
+	secondary_color_group.find(".color-circle").first().addClass("large-color-circle");
+
+	//knob data
 	$('.loader').knob({
 		'readOnly':true,
 		'width':"200",
@@ -66,7 +83,7 @@ $(document).ready(function() {
 
 });
 
-//recursive, calls itself every second
+//recursive, calls itself every second. This is a mess.
 function incr_loader(step){
 	num_seconds = 20;
 	if (step <= num_seconds){
@@ -77,15 +94,24 @@ function incr_loader(step){
 			setTimeout(incr_loader(step+1, num_seconds), 1000);
 		};
 	}
+	else{
+		setCenterCircle(secondary_color);
+	}
 }
 
 //takes in a color and a template, gets its opposite, saves that and uses it in template
 function setOppositeColor(color, template){
 	var opposite_color = _.find(secondary_color_data.colors, function(color){return color.name == "opposite";});
 	opposite_color.val=oppositeColor(color);
-	$("#secondary-colors").html(
-		template(secondary_color_data)
-	);
+	
+	var opposite_element = $("#secondary-colors").find(".color-circle").last();
+	opposite_element.css("background-color",opposite_color.val);
+
+	//var focused_el = $("#secondary-colors").find(".large-color-circle");
+	//$("#secondary-colors").html(
+	//	template(secondary_color_data)
+	//);
+	//$(focused_el).addClass("large-color-circle");
 }
 
 //takes in a color string, and returns an opposite color
@@ -103,6 +129,6 @@ function oppositeColor(in_color){
 	return out_color;
 }
 
-function setCenterCircle(){
-	$(".main-circle").css("background-color",primary_color);
+function setCenterCircle(color){
+	$(".main-circle").css("background-color",color);
 }
