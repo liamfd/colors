79fbf243088d3521f0b9807dcbad5c701d09d23e
liamfd <liamfd@gmail.com>
2014-05-08 23:04:24 -0400

    fixed opposite color updating when choosing individual color, tightened name - color relationship (get val from it)

--- js/templating.js
+++ js/templating.js
@@ -12,8 +12,8 @@ var secondary_color_data = { colors: [
 	{name: "opposite", val:"#009999"}
 ]};
 var test;
-var primary_color;
-var secondary_color;
+var primary_color_name;
+var secondary_color_name;
 
 
 $(document).ready(function() {
@@ -27,25 +27,28 @@ $(document).ready(function() {
     );
 
     //set default color values in js and DOM
-    primary_color = primary_color_data.colors[0].val;
-    secondary_color = secondary_color_data.colors[0].val;
-	setOppositeColor(primary_color, template);
-    setCenterCircle(primary_color);
+    primary_color_name = primary_color_data.colors[0].name;
+    secondary_color_name = secondary_color_data.colors[0].name;
+	setOppositeColor(getPrimaryColor(primary_color_name), template);
+    setCenterCircle(getPrimaryColor(primary_color_name).val);
+
+
 
     //click template and set click listener for primary
 	primary_color_group.html(
 		template(primary_color_data)
 	).on("click",".color-circle", function(e){
 		var el = e.target;
-		primary_color = el.dataset.color;
+		primary_color_name = el.dataset.name;
+		//primary_color = _.find(primary_color_data.colors, function(color){return color.name == primary_color_name;}).val;
 
 		//set visual styles of chosen color circles
 		primary_color_group.find(".color-circle").removeClass("large-color-circle");
 		$(el).addClass("large-color-circle");
 
 		//save the color, set the center circle color
-		setOppositeColor(primary_color, template);
-		setCenterCircle(primary_color);
+		setOppositeColor(getPrimaryColor(primary_color_name), template);
+		setCenterCircle(getPrimaryColor(primary_color_name).val);
 	});
 
 	//template and set click listener for secondary
@@ -53,7 +56,8 @@ $(document).ready(function() {
 		template(secondary_color_data)
 	).on("click",".color-circle", function(e){
 		var el = e.target;
-		secondary_color = el.dataset.color;
+		secondary_color_name = el.dataset.name;
+		//secondary_color = _.find(secondary_color_data.colors, function(color){return color.name == secondary_color_name;}).val;
 
 		//set visual styles of chosen color circles
 		secondary_color_group.find(".color-circle").removeClass("large-color-circle");
@@ -63,6 +67,8 @@ $(document).ready(function() {
 	//set default color options visually for the first elements of each list
 	primary_color_group.find(".color-circle").first().addClass("large-color-circle");
 	secondary_color_group.find(".color-circle").first().addClass("large-color-circle");
+
+
 
 	//knob data
 	$('.loader').knob({
@@ -85,7 +91,7 @@ $(document).ready(function() {
 
 //recursive, calls itself every second. This is a mess.
 function incr_loader(step){
-	num_seconds = 20;
+	num_seconds = 1;
 	if (step <= num_seconds){
 		return function(){
 			place = (step/num_seconds)*100;
@@ -95,26 +101,26 @@ function incr_loader(step){
 		};
 	}
 	else{
-		setCenterCircle(secondary_color);
+		setCenterCircle(getSecondaryColor(secondary_color_name).val);
 	}
 }
 
 //takes in a color and a template, gets its opposite, saves that and uses it in template
 function setOppositeColor(color, template){
 	var opposite_color = _.find(secondary_color_data.colors, function(color){return color.name == "opposite";});
-	opposite_color.val=oppositeColor(color);
+
+	opposite_color.val = oppositeColor(color.val);
 	
-	var opposite_element = $("#secondary-colors").find(".color-circle").last();
+	var opposite_element = $("#secondary-colors").find(".color-circle").last(); //opposite is always last
+	console.log(opposite_element);
 	opposite_element.css("background-color",opposite_color.val);
 
-	//var focused_el = $("#secondary-colors").find(".large-color-circle");
 	//$("#secondary-colors").html(
 	//	template(secondary_color_data)
 	//);
-	//$(focused_el).addClass("large-color-circle");
 }
 
-//takes in a color string, and returns an opposite color
+//takes in a color string value, and returns an opposite color
 function oppositeColor(in_color){
 	var max_color_val = 255;
 	var out_color = "#";
@@ -131,4 +137,14 @@ function oppositeColor(in_color){
 
 function setCenterCircle(color){
 	$(".main-circle").css("background-color",color);
+}
+
+
+//return color object, not value
+function getPrimaryColor(color_name){
+	return _.find(primary_color_data.colors, function(color){return color.name == color_name;});
+}
+
+function getSecondaryColor(color_name){
+	return _.find(secondary_color_data.colors, function(color){return color.name == color_name;});
 }
